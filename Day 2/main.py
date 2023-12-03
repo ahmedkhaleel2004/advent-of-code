a45374efa65ee5aa9857f55bdd5b922327cd36cb
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-02 23:16:00 -0500

    Day 2 Puzzle 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -17,10 +17,41 @@ for line in lines:
             count = int(re.findall('\d+', colour)[0]) # regex
             nextLine = False
             if 'red' in colour and count > 12: nextLine = True; break
-            if 'blue' in colour and count > 14: nextLine = True; break
             if 'green' in colour and count > 13: nextLine = True; break
+            if 'blue' in colour and count > 14: nextLine = True; break
         if nextLine: break
     if nextLine: continue
     sumID += int(re.findall('\d+', this_game)[0])
 
 print(f"Part 1: {sumID}")
+
+'''
+*********************************************************
+
+PART 2
+
+*********************************************************
+'''
+
+sumSets = 0
+
+for line in lines:
+    max_red = 0
+    max_green = 0
+    max_blue = 0
+    line = line.split(':')
+    this_game = line.pop(0)
+    line = line[0]
+    line = line.split(';')
+    for i in range(len(line)):
+        line[i] = line[i].split(',')
+    for draw in line:
+        for colour in draw:
+            count = int(re.findall('\d+', colour)[0]) # regex
+            nextLine = False
+            if 'red' in colour: max_red = max(max_red, count)
+            if 'green' in colour: max_green = max(max_green, count)
+            if 'blue' in colour: max_blue = max(max_blue, count)
+    sumSets += max_red * max_green * max_blue
+
+print(f"Part 2: {sumSets}")

ef2cc1b029973bf9165053da1858cf2147dd0592
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-04 08:34:28 -0500

    Day 4 Puzzle 2

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,9 +1,13 @@
 with open('input.txt', 'r') as file:
     lines = [line.strip() for line in file]
 
-total = 0
+m = {} # lets keep track of the count of every card
 
-for line in lines:
+for idx, line in enumerate(lines): # easy card number
+
+    if idx not in m: # first case and if not from prev card
+        m[idx] = 1
+
     line = line.split(':')[1]
     line = line.split()
     i = 0
@@ -20,13 +24,14 @@ for line in lines:
     for i in my:
         if i in winning:
             count += 1
-    if count == 1:
-        score = 1
-    elif count > 1:
-        score = 2**(count-1)
-    else:
-        score = 0
     
-    total += score
+    for n in range(idx + 1, idx + count + 1): # for next up till i + count
+        m[n] = m.get(n, 1) + m[idx] # update the value in the map, with default just incase
+        # we add m[i] because the number of times we have the current card
+        # is the number of times we need to add to the next card check
+        # absolutely genius
 
-print(total)
+# part 2 logic from hyper neutrino
+# https://www.youtube.com/watch?v=uxShpk__9xE&ab_channel=HyperNeutrino
+
+print(sum(m.values()))

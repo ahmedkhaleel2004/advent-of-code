08e85a37c7f3a1677c187d245cfa992be5ae38de
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-04 07:29:11 -0500

    Day 3 Puzzle 2

--- Day 3/main.py
+++ Day 3/main.py
@@ -5,12 +5,15 @@ with open('input.txt', 'r') as file:
 # very simple and easy solution
 # https://www.youtube.com/watch?v=ZFGX5D9mi-4&ab_channel=HyperNeutrino
 
-cs = set()
+total = 0
 
 for r, row in enumerate(lines):
     for c, ch in enumerate(row):
-        if ch.isdigit() or ch == ".":
+        if ch != '*':
             continue
+
+        cs = set()
+
         for dr in range(r - 1, r + 2):
             for dc in range(c - 1, c + 2):
                 if dr < 0 or dr >= len(lines) or dc < 0 or dc >= len(lines[dr]) or not lines[dr][dc].isdigit():
@@ -19,13 +22,18 @@ for r, row in enumerate(lines):
                     dc -= 1
                 cs.add((dr, dc))
 
-ns = []
+        if len(cs) != 2:
+            continue
 
-for r, c in cs:
-    s = ""
-    while c < len(lines[r]) and lines[r][c].isdigit():
-        s += lines[r][c]
-        c += 1
-    ns.append(int(s))
+        ns = []
 
-print(sum(ns))
+        for cr, cc in cs:
+            s = ""
+            while cc < len(lines[cr]) and lines[cr][cc].isdigit():
+                s += lines[cr][cc]
+                cc += 1
+            ns.append(int(s))
+        
+        total += ns[0] * ns[1]
+
+print(total)

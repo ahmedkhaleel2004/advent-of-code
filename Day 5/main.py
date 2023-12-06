00165a6d78fb3be34a82e484c4ce8a9f981994d0
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-06 04:38:41 -0500

    Day 5 Puzzle 2

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,21 +1,34 @@
 # hyper neutrino solution... so simple its genius
 
-seeds, *blocks = open(0).read().split("\n\n")
+inputs, *blocks = open(0).read().split("\n\n")
 
-seeds = list(map(int,seeds.split(':')[1].split()))
+inputs = list(map(int,inputs.split(':')[1].split()))
+
+seeds = []
+
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i] + inputs[i + 1]))
 
 for block in blocks:
     ranges = []
     for line in block.splitlines()[1:]:
         ranges.append(list(map(int, line.split())))
     new = [] # update map
-    for x in seeds:
+    while len(seeds) > 0:
+        # think about ranges
+        s, e = seeds.pop()
         for a, b, c in ranges:
-            if b <= x < b + c: # if seed in range
-                new.append(x - b + a) # difference plus the start
+            os = max(s, b)
+            oe = min(e, b + c)
+            if os < oe:
+                new.append((os - b + a, oe - b + a))
+                if os > s:
+                    seeds.append((s, os))
+                if e > oe:
+                    seeds.append((oe, e))
                 break
-        else: # for-else: if it was not in the range, it simply maps to itself
-            new.append(x)
+        else:
+            new.append((s, e))
     seeds = new # update for next block
 
-print(min(seeds))
+print(min(seeds)[0])

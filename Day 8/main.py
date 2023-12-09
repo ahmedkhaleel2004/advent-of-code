585cd9999a2979ce41ad79b72ceb7ce26d1d7ce6
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-09 00:27:17 -0500

    Day 8 Puzzle 2

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,5 +1,7 @@
 # studying for math exam, just watched hyper neutrino real quick
 
+from math import gcd
+
 steps, _, *rest = open(0).read().splitlines()
 
 network = {}
@@ -8,12 +10,37 @@ for line in rest:
     pos, targets = line.split(" = ")
     network[pos] = targets[1:-1].split(", ")
 
-step_count = 0
-current = "AAA"
+positions = [key for key in network if key.endswith("A")]
+cycles = []
 
-while current != "ZZZ":
-    step_count += 1
-    current = network[current][0 if steps[0] == "L" else 1]
-    steps = steps[1:] + steps[0]
+for current in positions:
+    cycle = []
 
-print(step_count)
+    current_steps = steps
+    step_count = 0
+    first_z = None
+
+    while True:
+        while step_count == 0 or not current.endswith("Z"):
+            step_count += 1
+            current = network[current][0 if current_steps[0] == "L" else 1]
+            current_steps = current_steps[1:] + current_steps[0]
+
+        cycle.append(step_count)
+
+        if first_z is None: # makes sense
+            first_z = current
+            step_count = 0
+        elif current == first_z:
+            break
+
+    cycles.append(cycle)
+
+nums = [cycle[0] for cycle in cycles]
+
+lcm = nums.pop() # this is actually crazy -> the output will only ever be multiples, x, where final lcm of arr is len path
+
+for num in nums:
+    lcm = lcm * num // gcd(lcm, num)
+
+print(lcm)

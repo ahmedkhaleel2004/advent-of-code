54d05e3d49b5c7758b5aebdab8729671a7305b27
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-15 00:44:27 -0500

    Day 15 Puzzle 2

--- Day 15/main.py
+++ Day 15/main.py
@@ -1,4 +1,5 @@
 # my personal solution
+import re
 
 words = open(0).read().split(",")
 
@@ -14,7 +15,24 @@ def hashfunc(step):
 
 total = 0
 
+# only thing that tripped me up was that the test case had "rn=1" in box 0 when it should be in box 30
+
+# had to look at a solution here to confirm box_idx labelling
+boxes = [{} for _ in range(256)]
 for word in words:
-    total += hashfunc(word)
+    label = re.findall("\w+", word)[0] # easiest way to do this
+    box_idx = hashfunc(label)
+    if "-" in word:
+        if label in boxes[box_idx]:
+            del boxes[box_idx][label] # remove that key pair
+    else:
+        focal_length = int(word.split("=")[1])
+        boxes[box_idx][label] = focal_length
+
+for i, box in enumerate(boxes):
+    power = 0
+    for j, focal_length in enumerate(box.values()):
+        power += (i + 1) * (j + 1) * focal_length
+    total += power
 
 print(total)

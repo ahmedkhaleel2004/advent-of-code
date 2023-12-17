c5b28c5ae46490e14ff398292b23ab588fb59ae4
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-17 08:51:53 -0500

    Day 16 Puzzle 2

--- Day 16/main.py
+++ Day 16/main.py
@@ -2,42 +2,57 @@ from collections import deque
 
 grid = open(0).read().splitlines()
 
-# r, c, dr, dc
-a = [(0, -1, 0, 1)]
-seen = set()
-q = deque(a)
+def calc(r, c, dr, dc): # wrap in func
+    # r, c, dr, dc
+    a = [(r, c, dr, dc)]
+    seen = set()
+    q = deque(a)
 
-while q:
-    r, c, dr, dc = q.popleft()
+    while q:
+        r, c, dr, dc = q.popleft()
 
-    r += dr
-    c += dc
+        r += dr
+        c += dc
 
-    if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
-        continue
+        if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+            continue
 
-    ch = grid[r][c]
-    
-    if ch == "." or (ch == "-" and dc != 0) or (ch == "|" and dr != 0):
-        if (r, c, dr, dc) not in seen:
-            seen.add((r, c, dr, dc))
-            q.append((r, c, dr, dc))
-    elif ch == "/":
-        dr, dc = -dc, -dr
-        if (r, c, dr, dc) not in seen:
-            seen.add((r, c, dr, dc))
-            q.append((r, c, dr, dc))
-    elif ch == "\\":
-        dr, dc = dc, dr
-        if (r, c, dr, dc) not in seen:
-            seen.add((r, c, dr, dc))
-            q.append((r, c, dr, dc))
-    else:
-        for dr, dc in [(1, 0), (-1, 0)] if ch == "|" else [(0, 1), (0, -1)]:
+        ch = grid[r][c]
+        
+        if ch == "." or (ch == "-" and dc != 0) or (ch == "|" and dr != 0):
             if (r, c, dr, dc) not in seen:
                 seen.add((r, c, dr, dc))
                 q.append((r, c, dr, dc))
-                
-coords = {(r, c) for (r, c, _, _) in seen}
+        elif ch == "/":
+            dr, dc = -dc, -dr
+            if (r, c, dr, dc) not in seen:
+                seen.add((r, c, dr, dc))
+                q.append((r, c, dr, dc))
+        elif ch == "\\":
+            dr, dc = dc, dr
+            if (r, c, dr, dc) not in seen:
+                seen.add((r, c, dr, dc))
+                q.append((r, c, dr, dc))
+        else:
+            for dr, dc in [(1, 0), (-1, 0)] if ch == "|" else [(0, 1), (0, -1)]:
+                if (r, c, dr, dc) not in seen:
+                    seen.add((r, c, dr, dc))
+                    q.append((r, c, dr, dc))
+                    
+    coords = {(r, c) for (r, c, _, _) in seen}
+    
+    return len(coords)
 
-print(len(coords))
+max_val = 0
+
+# now just iterate through all possible combinations
+
+for r in range(len(grid)):
+    max_val = max(max_val, calc(r, -1, 0, 1))
+    max_val = max(max_val, calc(r, len(grid[0]), 0, -1))
+    
+for c in range(len(grid)):
+    max_val = max(max_val, calc(-1, c, 1, 0))
+    max_val = max(max_val, calc(len(grid), c, -1, 0))
+
+print(max_val)

73ea0fed0535ed2d9335b2520e4cbb4de29aa0e6
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2023-12-19 03:15:14 -0500

    Day 18 Puzzle 2

--- Day 18/main.py
+++ Day 18/main.py
@@ -8,14 +8,18 @@ b = 0
 # then subtract boundary points and add them to inside points
 
 for line in open(0):
-    d, n, _ = line.split()
-    dr, dc = dirs[d]
-    n = int(n)
+    _, _, x = line.split()
+    x = x[2:-1]
+    dr, dc = dirs["RDLU"[int(x[-1])]] # use it as index
+    n = int(x[:-1], 16) # convert to base 10
     b += n
     r, c = points[-1]
     points.append((r + dr * n, c + dc * n))
 
 A = abs(sum(points[i][0] * (points[i - 1][1] - points[(i + 1) % len(points)][1]) for i in range(len(points)))) // 2
 i = A - b // 2 + 1
+
+# math solution is always the faster solution
+# this runs in same time
 
 print(i + b)

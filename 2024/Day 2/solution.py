9d4af3681fed5aaeaf27606fbc2a053378b56b88
Ahmed Khaleel <ahmedkhaleel2004@gmail.com>
2024-12-02 00:52:26 -0500

    Day 2 Part 2

--- 2024/Day 2/solution.py
+++ 2024/Day 2/solution.py
@@ -1,22 +1,41 @@
 lines = open(0).read().splitlines()
 ans = 0
 
+# wow this is like O(n^3) for part 2
+
 for row in lines:
-    safe = True
     nums = list(map(int, row.split()))
 
-    increasing = all(nums[i] < nums[i + 1] for i in range(len(nums) - 1))
-    decreasing = all(nums[i] > nums[i + 1] for i in range(len(nums) - 1))
+    def process_nums(nums):
+        safe = True
+        global ans
 
-    if not (increasing or decreasing):
-        continue
+        increasing = all(nums[i] < nums[i + 1] for i in range(len(nums) - 1))
+        decreasing = all(nums[i] > nums[i + 1] for i in range(len(nums) - 1))
 
-    for i in range(len(nums) - 1):
-        if not 1 <= abs(nums[i] - nums[i+1]) <= 3:
-            safe = False
-            break
+        if not (increasing or decreasing):
+            return False
 
-    if safe:
-        ans += 1
+        for i in range(len(nums) - 1):
+            print(
+                f"{nums} now trying {nums[i]} - {nums[i+1]} which is a diff of {abs(nums[i] - nums[i+1])}")
+            if not 1 <= abs(nums[i] - nums[i+1]) <= 3:
+                safe = False
+                break
+
+        if safe:
+            ans += 1
+
+        return safe
+
+    safe = process_nums(nums)
+
+    if not safe:
+        for i in range(len(nums)):
+            new = nums[:i] + nums[i+1:]
+            print(f"{nums} is unsafe, now passing {new}")
+            safe = process_nums(new)
+            if safe:
+                break
 
 print(ans)
